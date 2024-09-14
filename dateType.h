8374eaf541b9dd8a273a98878488ddfb3db8a0c9
jwp23325 <jwp23325@email.vccs.edu>
2024-09-14 17:05:57 -0400

    Lower cased addressType and dateType

---
+++ dateType.h
@@ -0,0 +1,104 @@
+// This code was outline using ChatGPT and modified by the programmer.
+
+#pragma once
+# include <iostream>
+
+
+
+class dateType
+{
+private:
+	int dMonth;
+	int dDay;
+	int dYear;
+
+public:
+	// Constructor with default parameters
+	DateType(int month = 1, int day = 1, int year = 1900) {
+		setDate(month, day, year);
+	}
+	
+	// Checks if year is a leap year
+	bool isLeapYear(int year) {
+		return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+	}
+
+	// Sets date with validtion
+	void setDate(int month, int day, int year) {
+
+		// Validates year
+		if (year >= 1900) {
+			dYear = year;
+		}
+
+		// Default year
+		else {
+			dYear = 1900;
+		}
+
+		// Validates month
+		if (month >= 1 && month <= 12) {
+			dMonth = month;
+		}
+
+		// Default month
+		else {
+			dMonth = 1;
+		}
+
+		// Maximum day in a month based on the year
+		int maxDay = getDaysInMonth(dMonth, dYear); {
+
+			// Validates day
+			if (day >= 1 && day <= maxDay) {
+				dDay = day;
+			}
+
+			// Default day
+			else {
+				dDay = 1;
+			}
+		}
+	}
+
+	// Gets the number of days in the month
+	int getDaysInMonth(int month, int year) {
+
+		// Control flow
+		switch(month) {
+			// January, March, May, July, August, October, December
+            case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+				return 31;
+
+			// April, June, September, November
+			case 4: case 6: case 9: case 11:
+				return 30;
+		
+			// Febuary 
+			case 2:
+				return isLeapYear(year) ? 29 : 28;
+
+		}
+	}
+
+	// Gets day
+	int getDay() {
+		return dDay;
+	}
+
+	// Gets month
+	int getMonth() {
+		return dMonth;
+	}
+
+	// Gets year
+	int getYear() {
+		return dYear;
+	}
+	
+	// Print method
+	void print() const {
+		std::cout << dMonth << "/" << dDay << "/" << dYear << std::endl;
+	}
+};
+
--- extPersonType.h
+++ extPersonType.h
@@ -1,5 +1,15 @@
 #pragma once
-class extPersonType
+#include "personType.h"
+#include "dateType.h"
+#include "addressType.h"
+#include <string>
+#include <iostream>
+
+class extPersonType : public personType
 {
+private:
+	dateType birthdate;
+	addressType address;
+
 };
 
