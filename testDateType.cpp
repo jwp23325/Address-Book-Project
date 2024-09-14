06938d2fdd0161515514d6c55a224de4c69d2a1a
jwp23325 <jwp23325@email.vccs.edu>
2024-09-14 14:21:12 -0400

    Added testDateType.cpp file

--- testDateType.cpp
+++ testDateType.cpp
@@ -1,9 +1,41 @@
-// This file tests the Address Type Class
+// Program to verify that the modified dateType class is working correctl
 
 #include <iostream>
-#include "DateType"
+#include "DateType.h"
 using namespace std;
+
 int main()
 {
-	
+	cout << "Testing default constructor ... ";
+	DateType defDate;
+	defDate.print();
+	cout << endl;
+	cout << "Testing constructor with parameters ... ";
+	DateType date(2, 29, 2016);
+	date.print();
+	cout << endl;
+	cout << "Setting date with invalid year (1000) ... ";
+	date.setDate(2, 29, 1000);
+	date.print();
+	cout << endl;
+	cout << "Setting date with invalid month (13) ... ";
+	date.setDate(13, 29, 2016);
+	date.print();
+	cout << endl;
+	cout << "Setting date with invalid day (4, 31) ... ";
+	date.setDate(4, 31, 2016);
+	date.print();
+	cout << endl;
+	cout << "Setting invalid date: Feb 29, 2015 ... ";
+	date.setDate(2, 29, 2015);
+	date.print();
+	cout << endl;
+	cout << "Setting valid date: Feb 28, 2015 ... ";
+	date.setDate(2, 28, 2015);
+	date.print();
+	cout << endl;
+	cout << "Setting valid date (August 15, 2016) ... ";
+	date.setDate(8, 15, 2016);
+	date.print();
+	cout << endl;
 }
