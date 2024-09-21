c19629e29ef4f398f8d7d96b0d1550bb9b363093
jwp23325 <jwp23325@email.vccs.edu>
2024-09-21 09:22:17 -0400

    Making separate .h files and .cpp files

--- AddressType.h
+++ AddressType.h
@@ -1,104 +1,39 @@
 // This code was outlined using ChatGPT and modified by the coder
-
-
 #pragma once
 #include <iostream>
 #include <string>
-#include <algorithm> 
-#include <vector> 
+#include <vector>
+#include <algorithm>
 
-
-class AddressType
-{
+class AddressType {
 private:
-	std::string address;
-	std::string city;
-	std::string state;
-	int zipcode;
+    std::string address;
+    std::string city;
+    std::string state;
+    int zipcode;
 
-	// List of valid state abbreviations
-	static const std::vector<std::string> validStates;
+    // List of valid state abbreviations
+    static const std::vector<std::string> validStates;
 
 public:
-	// Constructor with default parameters using initialization list
-	AddressType(std::string addr = "", std::string cty = "", std::string st = "XX", int zip = 10000)
-		: address(addr), city(cty), state(st), zipcode(zip) {
-		
-		// Validates state and zipcode
-		setState(st);
-		setZipcode(zip);
-	}
+    // Constructor with default parameters using initialization list
+    AddressType(std::string addr = "", std::string cty = "", std::string st = "XX", int zip = 10000);
 
-	// Sets the Address
-	void setAddress(std::string addr) {
-		address = addr;
-	}
+    // Setters
+    void setAddress(std::string addr);
+    void setCity(std::string cty);
+    void setState(std::string st);
+    void setZipcode(int zip);
 
-	// Sets the City
-	void setCity(std::string cty) { 
-		city = cty;
-	}
+    // Getters
+    std::string getAddress() const;
+    std::string getCity() const;
+    std::string getState() const;
+    int getZipcode() const;
 
-	// Sets the State
-	void setState(std::string st) {
-		// Validates state before setting
-		if (std::find(validStates.begin(), validStates.end(), st) != validStates.end()) {
-			state = st;
-		}
-		// Sets state to default value
-		else {
-			state = "XX";
-		}
-	}
-
-	// Sets the Zipcode
-	void setZipcode(int zip) {
-		// Validates zipcode before setting
-		if (zip >= 11111 && zip <= 99999) {
-			zipcode = zip;
-		}
-		// Sets zipcode to default value
-		else {
-			zipcode = 10000;
-		}
-	}
-
-	// Gets the address
-	std::string getAddress() const {
-		return address;
-	}
-
-	// Gets the city
-	std::string getCity() const {
-		return city;
-	}
-
-	// Gets the state
-	std::string getState() const {
-		return state;
-	}
-
-	// Gets the zipcode
-	int getZipcode() const {
-		return zipcode;
-	}
-
-	// Print metho
-	void print() const {
-		std::cout << address << std::endl
-				  << city << ", " << state << " "
-			      << zipcode << std::endl;
-	}
-	
-
+    // Print method
+    void print() const;
 };
 
-// Define the list of valid state abbreviations
-const std::vector<std::string> AddressType::validStates = {
-	"AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID",
-	"IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS",
-	"MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK",
-	"OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV",
-	"WI", "WY"
-};
+
 
--- testAddressType.cpp
+++ testAddressType.cpp
@@ -1,31 +1,37 @@
-// This file tests the Address Type Class
-
+#pragma once
 #include <iostream>
 #include "AddressType.h"
+
 using namespace std;
-int main()
-{
-	cout << "Testing default constructor ... " << endl;
-	AddressType defAddress;
-	defAddress.print();
-	cout << endl;
-	cout << "Testing constructor with parameters ... " << endl;
-	AddressType address("123 South Street", "Newport News", "VA", 23664);
-	address.print();
-	cout << endl;
-	cout << "Testing invalid state (Virginia)... " << endl;
-	address.setState("Virginia");
-	address.print();
-	cout << endl;
-	cout << "Testing invalid zipcode (55555555)..." << endl;
-	address.setZipcode(55555555);
-	address.print();
-	cout << endl;
-	cout << "Testing valid address ..." << endl;
-	address.setAddress("44 East Main Street");
-	address.setCity("Hampton");
-	address.setState("VA");
-	address.setZipcode(23669);
-	address.print();
-	cout << endl;
+
+int main() {
+    cout << "Testing default constructor ... " << endl;
+    AddressType defAddress;
+    defAddress.print();
+    cout << endl;
+
+    cout << "Testing constructor with parameters ... " << endl;
+    AddressType address("123 South Street", "Newport News", "VA", 23664);
+    address.print();
+    cout << endl;
+
+    cout << "Testing invalid state (Virginia)... " << endl;
+    address.setState("Virginia");
+    address.print();
+    cout << endl;
+
+    cout << "Testing invalid zipcode (55555555)..." << endl;
+    address.setZipcode(55555555);
+    address.print();
+    cout << endl;
+
+    cout << "Testing valid address ..." << endl;
+    address.setAddress("44 East Main Street");
+    address.setCity("Hampton");
+    address.setState("VA");
+    address.setZipcode(23669);
+    address.print();
+    cout << endl;
+
+    return 0;
 }
