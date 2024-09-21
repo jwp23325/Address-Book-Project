ff4ed1bf083772b8ef5d1124becd11260b10f096
jwp23325 <jwp23325@email.vccs.edu>
2024-09-21 15:45:25 -0400

    Formatted comments for extPersontType.h

--- extPersonType.h
+++ extPersonType.h
@@ -12,37 +12,31 @@
 
 class extPersonType : public PersonType {
 private:
-    // Composition: dateType and addressType objects
-    DateType birthdate;
-    AddressType address;
 
-    // Additional member variables
-    std::string phoneNumber;
-    std::string relationship;
+    DateType birthdate;                                     // Birthdate: Composition of DateType object
+    AddressType address;                                    // Address:   Composition of AddressType object
+    std::string phoneNumber;                                // Phone number
+    std::string relationship;                               // Relationship
 
 public:
-    // Constructor with default parameters
-    extPersonType(const std::string& fName = "", const std::string& lName = "",
-        int month = 1, int day = 1, int year = 1900,
-        const std::string& street = "", const std::string& city = "",
-        const std::string& state = "XX", int zip = 10000,
-        const std::string& phone = "None", const std::string& relation = "None");
+    
+    extPersonType(const std::string& fName = "", 
+                  const std::string& lName = "",
+                  int month = 1, int day = 1, 
+                  int year = 1900,
+                  const std::string& street = "", 
+                  const std::string& city = "",
+                  const std::string& state = "XX", 
+                  int zip = 10000,
+                  const std::string& phone = "None", 
+                  const std::string& relation = "None");    // Constructor with default parameters
 
-    // Sets phone number
-    void setPhoneNumber(const std::string& phone);
+    void setPhoneNumber(const std::string& phone);          // Sets phone number
+    void setRelationship(const std::string& relation);      // Sets relationship
 
-    // Sets relationship
-    void setRelationship(const std::string& relation);
+    std::string getPhoneNumber() const;                     // Gets phone number
+    std::string getRelationship() const;                    // Gets relationhip    
+    int getBirthMonth() const;                              // Gets birth month
 
-    // Gets phone number
-    std::string getPhoneNumber() const;
-
-    // Gets relationhip
-    std::string getRelationship() const;
-
-    // Gets birth month
-    int getBirthMonth() const;
-
-    // Print function
-    void print() const;
+    void print() const;                                     // Print function
 };
