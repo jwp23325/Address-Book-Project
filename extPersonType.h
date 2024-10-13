20cb67aec741fc7741ddeda99662625f80dc64d4
jwp23325 <jwp23325@email.vccs.edu>
2024-10-13 17:49:34 -0400

    Formatting

--- extPersonType.h
+++ extPersonType.h
@@ -27,7 +27,7 @@ public:
         const std::string& state = "XX",
         int zip = 10000,
         const std::string& phone = "None",
-        const std::string& relation = "None");    // Constructor with default parameters
+        const std::string& relation = "None");              // Constructor with default parameters
 
     void setPhoneNumber(const std::string& phone);          // Sets phone number
     void setRelationship(const std::string& relation);      // Sets relationship
@@ -35,6 +35,14 @@ public:
     std::string getPhoneNumber() const;                     // Gets phone number
     std::string getRelationship() const;                    // Gets relationship    
     int getBirthMonth() const;                              // Gets birth month
+
+    void print() const;                                     // Print function
+
+    // Overloaded operators
+    bool operator==(const extPersonType& other) const;      // Overload == operator
+    bool operator!=(const extPersonType& other) const;      // Overload != operator
+    bool operator>=(const extPersonType& other) const;      // Overload >= operator
+
 
     // New getter functions for DateType
     int getBirthDay() const;                                // Gets birth day
@@ -45,13 +53,4 @@ public:
     std::string getCity() const;                            // Gets city
     std::string getState() const;                           // Gets state
     int getZipcode() const;                                 // Gets zip
-
-    void print() const;                                     // Print function
-
-    // Overloaded operators
-    bool operator==(const extPersonType& other) const;      // Overload == operator
-    bool operator!=(const extPersonType& other) const;      // Overload != operator
-    bool operator>=(const extPersonType& other) const;      // Overload >= operator
 };
-
-
